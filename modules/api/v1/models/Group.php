201721cfe4be68232c3c42b18ba13661faa939bb
Sharique <sharique.khan@yahoo.com>
2015-01-02 02:21:16 +0500

    Move group creation in model.

--- modules/api/v1/models/Group.php
+++ modules/api/v1/models/Group.php
@@ -1,11 +1,16 @@
 <?php
 
 namespace app\modules\api\v1\models;
+use \yii\base\Exception;
 
 use yii\db\ActiveRecord;
 
 class Group extends ActiveRecord
 {
+    
+    public $error_lst = array();
+    public $data = array();
+    public $success = false;
 	
 	/**
      * @inheritdoc
@@ -64,6 +69,25 @@ class Group extends ActiveRecord
         $value = strtolower(trim($this->$attribute));
         if($value != 'f' || $value != 't'){
             $this->addError($attribute, "Invalid entry");
+        }
+    }
+    
+    public function postGroup(){
+        if ($this->save()) {
+            $this->success= true;
+            $this->data = $this->id;
+                    
+            return array('success'=>$this->success ,'data'=>$this->data , 'errors'=>$this->error_lst);
+
+        } 
+        
+        else{
+                foreach($this->errors as $key => $value){
+                    array_push($this->error_lst, $value[0]);
+                }
+
+                return array('success'=>$this->success ,'data'=>$this->data , 'errors'=>$this->error_lst);
+
         }
     }
 	
