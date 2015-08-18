1ef68ccf13be906af6ab59069fc02d3624a6a6ed
Sharique <sharique.avenov@gmail.com>
2015-08-18 20:40:24 +0500

    Change structure of specialty

--- modules/api/v1/models/Specialty/Specialty.php
+++ modules/api/v1/models/Specialty/Specialty.php
@@ -20,18 +20,18 @@ class Specialty extends ActiveRecord
      */
     public static function primaryKey()
     {
-        return ['short_name'];
+        return ['short_code'];
     }
     
     public static function isSpecialtyExist($specialtyCode){
-        return self::find()->where("short_name = :name", 
-                        [":name" => $specialtyCode])->exists();
+        return self::find()->where("short_code = :code", 
+                        [":code" => $specialtyCode])->exists();
     }
     
     public function fields() {
         return [
             'name' => 'full_name',
-            'value' => 'short_name',
+            'value' => 'short_code',
             
         ];
     }
